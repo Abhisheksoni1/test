df49cf631f508634400de187ad0567bee59b472c
abhishek_ng-at-737121988715 <abhi@neargroup.me>
2019-12-10 00:56:53 +0530

    test

--- browser/assets/js/custom.js
+++ browser/assets/js/custom.js
@@ -15,7 +15,42 @@
  
 
 // }
+$(document).ready( function() {
+    $('#myCarousel').carousel({
+    	interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#myCarousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+});
+$(document).click(function() {
+    $('.srch_blog_list').hide();
+    $('.srch_input_field').val('');
+    $('.srch_blog_nolist').hide();
+});
 
-
-
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5dd52eb943be710e1d1e3eeb/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 
